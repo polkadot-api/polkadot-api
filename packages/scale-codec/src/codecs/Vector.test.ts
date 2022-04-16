8bbe27e97b5484e3d6f2950dbc5ec3e87418dd72
Josep M Sobrepere <jm.sobrepere@gmail.com>
2022-04-16 21:58:40 +0200

    feat: support fixed siz Vectors

--- packages/scale-codec/src/codecs/Vector.test.ts
+++ packages/scale-codec/src/codecs/Vector.test.ts
@@ -1,12 +1,18 @@
 import { Vector, u16 } from "../"
 import { testCodec } from "../test-utils"
 
-const tester = testCodec(Vector(u16))
-
 describe("Vector", () => {
-  it("works", () => {
+  it("Sequence", () => {
+    const testerSeq = testCodec(Vector(u16))
     const value = [4, 8, 15, 16, 23, 42]
     const hex = "0x18040008000f00100017002a00"
-    tester(value, hex)
+    testerSeq(value, hex)
+  })
+
+  it("Array", () => {
+    const testerArr = testCodec(Vector(u16, 5))
+    const value = [4, 8, 15, 16, 23]
+    const hex = "0x040008000f0010001700"
+    testerArr(value, hex)
   })
 })
--- packages/scale-codec/src/codecs/Vector.ts
+++ packages/scale-codec/src/codecs/Vector.ts
@@ -4,14 +4,14 @@ import { createCodec } from "../utils"
 import { Codec, Decoder, Encoder } from "../types"
 import { compact } from "./compact"
 
-function VectorEnc<T>(inner: Encoder<T>): Encoder<Array<T>> {
-  return (value: Array<any>) =>
-    mergeUint8(compact.enc(value.length), ...value.map(inner))
-}
+const VectorEnc = <T>(inner: Encoder<T>, size?: number): Encoder<Array<T>> =>
+  size! > 0
+    ? (value) => mergeUint8(...value.map(inner))
+    : (value) => mergeUint8(compact.enc(value.length), ...value.map(inner))
 
-function VectorDec<T>(getter: Decoder<T>): Decoder<Array<T>> {
+function VectorDec<T>(getter: Decoder<T>, size?: number): Decoder<Array<T>> {
   return toInternalBytes((bytes) => {
-    let nElements = compact.dec(bytes)
+    const nElements = size! > 0 ? size! : compact.dec(bytes)
     const result = new Array(nElements as number)
 
     for (let i = 0; i < nElements; i++) {
@@ -23,8 +23,8 @@ function VectorDec<T>(getter: Decoder<T>): Decoder<Array<T>> {
   })
 }
 
-export function Vector<T>(inner: Codec<T>): Codec<Array<T>> {
-  return createCodec(VectorEnc(inner[0]), VectorDec(inner[1]))
+export function Vector<T>(inner: Codec<T>, size?: number): Codec<Array<T>> {
+  return createCodec(VectorEnc(inner[0], size), VectorDec(inner[1], size))
 }
 
 Vector.enc = VectorEnc
