ef02660e3694e412c9613491f2276fed58b0baaa
Josep M Sobrepere <jm.sobrepere@gmail.com>
2022-04-18 22:21:07 +0200

    improve Option typings

--- packages/scale-ts/src/codecs/Option.ts
+++ packages/scale-ts/src/codecs/Option.ts
@@ -4,7 +4,7 @@ import { createCodec } from "../utils"
 import { u8 } from "./u8"
 import { bool } from "./bool"
 
-const OptionDec = <T>(inner: Decoder<T>): Decoder<T | undefined> =>
+const OptionDec = <T>(inner: Decoder<T>): Decoder<T | undefined | void> =>
   toInternalBytes<T | undefined>((bytes) => {
     const val = u8.dec(bytes)
     if (val === 0) return undefined
@@ -15,7 +15,7 @@ const OptionDec = <T>(inner: Decoder<T>): Decoder<T | undefined> =>
   })
 
 const OptionEnc =
-  <T>(inner: Encoder<T>): Encoder<T | undefined> =>
+  <T>(inner: Encoder<T>): Encoder<T | undefined | void> =>
   (value) => {
     const result = new Uint8Array(1)
     if (value === undefined) {
@@ -32,7 +32,7 @@ const OptionEnc =
     return mergeUint8(result, inner(value))
   }
 
-export const Option = <T>(inner: Codec<T>): Codec<T | undefined> =>
+export const Option = <T>(inner: Codec<T>): Codec<T | undefined | void> =>
   createCodec(OptionEnc(inner[0]), OptionDec(inner[1]))
 
 Option.enc = OptionEnc
