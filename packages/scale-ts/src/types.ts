9882343e1472831b872a6e6889aa9a2f1139e022
Josep M Sobrepere <jm.sobrepere@gmail.com>
2022-04-22 00:06:40 +0200

    scale-ts: nit in types.ts

--- packages/scale-ts/src/types.ts
+++ packages/scale-ts/src/types.ts
@@ -1,5 +1,5 @@
-export type Decoder<T> = (value: Uint8Array | ArrayBuffer | string) => T
 export type Encoder<T> = (value: T) => Uint8Array
+export type Decoder<T> = (value: Uint8Array | ArrayBuffer | string) => T
 
 export type Codec<T> = [Encoder<T>, Decoder<T>] & {
   enc: Encoder<T>
