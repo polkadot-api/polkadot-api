022475e2b799286fde767f39facc35a106cbba85
Josep M Sobrepere <jm.sobrepere@gmail.com>
2023-07-02 16:16:05 +0200

    scale-ts: tiny perf improvement

--- packages/scale-ts/src/internal/mapObject.ts
+++ packages/scale-ts/src/internal/mapObject.ts
@@ -7,9 +7,14 @@ export function mapObject<K extends string | number | symbol, I, O>(
   input: Record<K, I>,
   mapper: (i: I, k?: K) => O,
 ): Record<K, O> {
-  return Object.fromEntries(
-    Object.entries(input).map(
-      ([key, value]: any) => [key, mapper(value, key)] as const,
-    ),
-  ) as any
+  const keys = Object.keys(input) as Array<K>
+  const len = keys.length
+
+  const result: Record<K, O> = {} as any
+  for (let i = 0; i < len; i++) {
+    const key = keys[i]
+    result[key] = mapper(input[key], key)
+  }
+
+  return result
 }
