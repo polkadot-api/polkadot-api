1f8d0388f9a362c3b41e7fe44a867977036791b0
Ryan Lee <ryan@parity.io>
2023-08-02 11:27:30 -0400

    refactor: substrate bindings storage

--- packages/substrate-bindings/src/storage.ts
+++ packages/substrate-bindings/src/storage.ts
@@ -1,11 +1,15 @@
 import { mergeUint8, toHex, utf16StrToUtf8Bytes } from "@unstoppablejs/utils"
 import { Decoder, Encoder } from "@unstoppablejs/substrate-codecs"
 import { Twox128 } from "./hashes"
+import type { SetReturnType } from "type-fest"
 
-type EncoderWithHash<T> = [Encoder<T>, (input: Uint8Array) => Uint8Array]
+export type EncoderWithHash<T> = [Encoder<T>, (input: Uint8Array) => Uint8Array]
+export type StorageCodec<OT, T> = {
+  enc: SetReturnType<Encoder<OT>, string>
+  dec: Decoder<T>
+}
 
 export const Storage = (pallet: string) => {
-  const palledEncoded = Twox128(utf16StrToUtf8Bytes(pallet))
   return <
     T,
     A extends Array<EncoderWithHash<any>>,
@@ -16,12 +20,9 @@ export const Storage = (pallet: string) => {
     name: string,
     dec: Decoder<T>,
     ...encoders: [...A]
-  ): {
-    enc: (...args: OT) => string
-    dec: Decoder<T>
-  } => {
+  ): StorageCodec<OT, T> => {
     const palletItemEncoded = mergeUint8(
-      palledEncoded,
+      Twox128(utf16StrToUtf8Bytes(pallet)),
       Twox128(utf16StrToUtf8Bytes(name)),
     )
     const fns = encoders.map(
@@ -30,13 +31,14 @@ export const Storage = (pallet: string) => {
           hash(enc(val)),
     )
 
-    const enc = (...args: OT): string =>
-      toHex(
-        mergeUint8(palletItemEncoded, ...args.map((val, idx) => fns[idx](val))),
-      )
-
     return {
-      enc,
+      enc: (...args) =>
+        toHex(
+          mergeUint8(
+            palletItemEncoded,
+            ...args.map((val, idx) => fns[idx](val)),
+          ),
+        ),
       dec,
     }
   }
