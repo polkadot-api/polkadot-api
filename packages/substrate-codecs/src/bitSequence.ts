60e270a826bfc4baa6e090888a712c5d4026cad2
Josep M Sobrepere <jm.sobrepere@gmail.com>
2023-08-05 23:18:19 +0200

    fix: `BitSquence` -> `BitSequence`

--- packages/substrate-codecs/src/bitSequence.ts
+++ packages/substrate-codecs/src/bitSequence.ts
@@ -1,19 +1,19 @@
 import { Bytes, Decoder, Encoder, createCodec, createDecoder } from "scale-ts"
 import { compactNumber } from "./compact"
 
-export interface BitSquence {
+export interface BitSequence {
   bitsLen: number
   bytes: Uint8Array
 }
 
-const bitSequenceDecoder: Decoder<BitSquence> = createDecoder((data) => {
+const bitSequenceDecoder: Decoder<BitSequence> = createDecoder((data) => {
   const bitsLen = compactNumber.dec(data)
   const bytesLen = Math.ceil(bitsLen / 8)
   const bytes = Bytes(bytesLen).dec(data)
   return { bytes, bitsLen }
 })
 
-const bitSequenceEncoder: Encoder<BitSquence> = (input) => {
+const bitSequenceEncoder: Encoder<BitSequence> = (input) => {
   const lenEncoded = compactNumber.enc(input.bitsLen)
   const result = new Uint8Array(input.bytes.length + lenEncoded.length)
   result.set(lenEncoded, 0)
