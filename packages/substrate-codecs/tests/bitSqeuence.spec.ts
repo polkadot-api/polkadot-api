e12ec7442b90f666637103f1bc2df11eda5f44a3
Josep M Sobrepere <jm.sobrepere@gmail.com>
2023-08-05 08:07:30 +0200

    substrate-codecs: basic tests for `bitSequence`

---
+++ packages/substrate-codecs/tests/bitSqeuence.spec.ts
@@ -0,0 +1,26 @@
+import { concatBytes, randomBytes } from "@noble/hashes/utils"
+import { compact } from "scale-ts"
+import { describe, expect, test } from "vitest"
+import { bitSequence } from "@/."
+
+describe("bitSequence", () => {
+  test("it correctly encodes/decodes its inputs while only taking the necessary bytes", () => {
+    // 9 bits required 2 bytes
+    const bitsLen = 9
+    const nine = compact.enc(bitsLen)
+
+    const otherData = randomBytes(10)
+    const expectedBytes = otherData.slice(0, 2)
+    const input = concatBytes(nine, otherData)
+
+    const decoded = bitSequence.dec(input)
+
+    expect(decoded).toEqual({
+      bitsLen,
+      bytes: expectedBytes,
+    })
+
+    const encoded = bitSequence.enc(decoded)
+    expect(encoded).toEqual(input.slice(0, nine.length + expectedBytes.length))
+  })
+})
