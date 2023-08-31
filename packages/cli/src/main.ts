2c30ccb7e5778635a9acbb8e3a230aae405bc2b3
Ryan Lee <ryan@parity.io>
2023-08-31 01:45:53 -0400

    finish discrpenancies

--- packages/cli/src/main.ts
+++ packages/cli/src/main.ts
@@ -263,7 +263,7 @@ while (!exit) {
 
       const discrepancies: Array<{
         pallet: string
-        type: "constant" | "storage"
+        type: "constant" | "storage" | "event" | "error" | "extrinsic"
         name: string
         oldChecksum: bigint | null
         newChecksum: bigint | null
@@ -280,11 +280,61 @@ while (!exit) {
             pallet,
             constantName,
           )
-          if (newChecksum === null || checksum != newChecksum) {
+          if (checksum != newChecksum) {
             discrepancies.push({
               pallet,
               type: "constant",
               name: constantName,
+              oldChecksum: checksum,
+              newChecksum: newChecksum,
+            })
+          }
+        }
+        for (const [storageName, checksum] of Object.entries(storage ?? {})) {
+          const newChecksum = checksumBuilder.buildStorage(pallet, storageName)
+          if (checksum != newChecksum) {
+            discrepancies.push({
+              pallet,
+              type: "storage",
+              name: storageName,
+              oldChecksum: checksum,
+              newChecksum: newChecksum,
+            })
+          }
+        }
+        for (const [eventName, checksum] of Object.entries(events ?? {})) {
+          const newChecksum = checksumBuilder.buildEvent(pallet, eventName)
+          if (checksum != newChecksum) {
+            discrepancies.push({
+              pallet,
+              type: "event",
+              name: eventName,
+              oldChecksum: checksum,
+              newChecksum: newChecksum,
+            })
+          }
+        }
+        for (const [errorName, checksum] of Object.entries(errors ?? {})) {
+          const newChecksum = checksumBuilder.buildError(pallet, errorName)
+          if (checksum != newChecksum) {
+            discrepancies.push({
+              pallet,
+              type: "error",
+              name: errorName,
+              oldChecksum: checksum,
+              newChecksum: newChecksum,
+            })
+          }
+        }
+        for (const [callName, { checksum }] of Object.entries(
+          extrinsics ?? {},
+        )) {
+          const newChecksum = checksumBuilder.buildCall(pallet, callName)
+          if (checksum != newChecksum) {
+            discrepancies.push({
+              pallet,
+              type: "extrinsic",
+              name: callName,
               oldChecksum: checksum,
               newChecksum: newChecksum,
             })
@@ -303,11 +353,7 @@ while (!exit) {
         "Old Checksum":
           oldChecksum === null ? chalk.red(oldChecksum) : oldChecksum,
         "New Checksum":
-          newChecksum !== oldChecksum
-            ? chalk.magenta(newChecksum)
-            : newChecksum === null
-            ? chalk.red(newChecksum)
-            : newChecksum,
+          newChecksum === null ? chalk.red(newChecksum) : newChecksum,
       })
 
       console.log("-------- Constant Discrepancies --------")
@@ -315,6 +361,38 @@ while (!exit) {
         asTable(
           discrepancies
             .filter(({ type }) => type === "constant")
+            .map(mapDiscrepancy),
+        ),
+      )
+      console.log("-------- Storage Discrepancies --------")
+      console.log(
+        asTable(
+          discrepancies
+            .filter(({ type }) => type === "storage")
+            .map(mapDiscrepancy),
+        ),
+      )
+      console.log("-------- Event Discrepancies --------")
+      console.log(
+        asTable(
+          discrepancies
+            .filter(({ type }) => type === "event")
+            .map(mapDiscrepancy),
+        ),
+      )
+      console.log("-------- Error Discrepancies --------")
+      console.log(
+        asTable(
+          discrepancies
+            .filter(({ type }) => type === "error")
+            .map(mapDiscrepancy),
+        ),
+      )
+      console.log("-------- Extrinsic Discrepancies --------")
+      console.log(
+        asTable(
+          discrepancies
+            .filter(({ type }) => type === "extrinsic")
             .map(mapDiscrepancy),
         ),
       )
@@ -328,18 +406,92 @@ while (!exit) {
       }
 
       for (const discrepancy of discrepancies) {
-        if (discrepancy.newChecksum === null) {
-          switch (discrepancy.type) {
-            case "constant": {
-              const constants = descriptors[discrepancy.pallet].constants
-              if (constants) {
-                delete constants[discrepancy.name]
-              }
+        switch (discrepancy.type) {
+          case "constant": {
+            const { constants } = descriptors[discrepancy.pallet]
+            if (!constants) {
               break
             }
-            default:
-              break
+            if (discrepancy.newChecksum === null) {
+              delete constants[discrepancy.name]
+            } else {
+              constants[discrepancy.name] = discrepancy.newChecksum
+            }
+            break
           }
+          case "storage": {
+            const { storage } = descriptors[discrepancy.pallet]
+            if (!storage) {
+              break
+            }
+            if (discrepancy.newChecksum === null) {
+              delete storage[discrepancy.name]
+            } else {
+              storage[discrepancy.name] = discrepancy.newChecksum
+            }
+            break
+          }
+          case "event": {
+            const { events } = descriptors[discrepancy.pallet]
+            if (!events) {
+              break
+            }
+            if (discrepancy.newChecksum === null) {
+              delete events[discrepancy.name]
+            } else {
+              events[discrepancy.name] = discrepancy.newChecksum
+            }
+            break
+          }
+          case "error": {
+            const { errors } = descriptors[discrepancy.pallet]
+            if (!errors) {
+              break
+            }
+            if (discrepancy.newChecksum === null) {
+              delete errors[discrepancy.name]
+            } else {
+              errors[discrepancy.name] = discrepancy.newChecksum
+            }
+            break
+          }
+          case "extrinsic": {
+            const { extrinsics } = descriptors[discrepancy.pallet]
+            if (!extrinsics) {
+              break
+            }
+            if (discrepancy.newChecksum === null) {
+              delete extrinsics[discrepancy.name]
+            } else {
+              const newEvents: Record<string, Set<string>> = extrinsics[
+                discrepancy.name
+              ].events
+              for (const d of discrepancies.filter((d) => d.type === "event")) {
+                newEvents[d.pallet] = newEvents[d.pallet] ?? {}
+                if (d.newChecksum === null) {
+                  newEvents[d.pallet].delete(d.name)
+                }
+              }
+              const newErrors: Record<string, Set<string>> = extrinsics[
+                discrepancy.name
+              ].errors
+              for (const d of discrepancies.filter((d) => d.type === "error")) {
+                newErrors[d.pallet] = newErrors[d.pallet] ?? {}
+                if (d.newChecksum === null) {
+                  newErrors[d.pallet].delete(d.name)
+                }
+              }
+
+              extrinsics[discrepancy.name] = {
+                events: newEvents,
+                errors: newErrors,
+                checksum: discrepancy.newChecksum,
+              }
+            }
+            break
+          }
+          default:
+            break
         }
       }
 
