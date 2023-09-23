1767cdab6489db204711d70293ce824d27956456
Josep M Sobrepere <jm.sobrepere@gmail.com>
2023-09-23 15:10:15 +0200

    fix experiments

--- experiments/src/all-nominators.ts
+++ experiments/src/all-nominators.ts
@@ -1,4 +1,8 @@
-import { ScProvider, WellKnownChain } from "@polkadot-api/sc-provider"
+import {
+  ScProvider,
+  WellKnownChain,
+  GetProvider,
+} from "@polkadot-api/sc-provider"
 import { createClient } from "@polkadot-api/substrate-client"
 
 const smProvider = ScProvider(
@@ -8,22 +12,6 @@ const smProvider = ScProvider(
   },
 }*/,
 )
-
-export interface Provider {
-  send: (message: string) => void
-  open: () => void
-  close: () => void
-}
-export declare enum ProviderStatus {
-  ready = 0,
-  disconnected = 1,
-  halt = 2,
-}
-
-export declare type GetProvider = (
-  onMessage: (message: string) => void,
-  onStatus: (status: ProviderStatus) => void,
-) => Provider
 
 const withLogsProvider = (input: GetProvider): GetProvider => {
   return (onMsg, onStatus) => {
