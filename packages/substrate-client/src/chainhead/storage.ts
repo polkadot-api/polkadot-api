fd643ecc63121f9d0eb9d3bfefc7f3b630a77802
Ryan Lee <ryan@parity.io>
2023-09-25 14:54:37 -0400

    fix(substrate-client): storage type coercion

--- packages/substrate-client/src/chainhead/storage.ts
+++ packages/substrate-client/src/chainhead/storage.ts
@@ -1,4 +1,5 @@
-import { ClientRequest, OperationLimitError, StorageResult } from ".."
+import { OperationLimitError } from ".."
+import type { ClientRequest, FollowResponse, StorageResult } from ".."
 import type {
   CommonOperationEvents,
   LimitReached,
@@ -20,45 +21,83 @@ export const createStorageFn = (
     | OperationStorageDone
     | OperationWaitingForContinue
   >,
-) => {
+): FollowResponse["storage"] => {
   const cbStore = createStorageCb(request)
-  return abortablePromiseFn(
-    <Type extends StorageItemInput["type"]>(
-      resolve: (value: StorageResult<Type>) => void,
-      reject: (e: Error) => void,
-      hash: string,
-      type: Type,
-      key: string,
-      childTrie: string | null,
-    ) => {
-      const isDescendants = type.startsWith("descendants")
-      let result: any = isDescendants ? [] : null
+  return abortablePromiseFn((resolve, reject, hash, type, key, childTrie) => {
+    const result: {
+      value: StorageResult<"value">
+      hash: StorageResult<"hash">
+      closestDescendantMerkleValue: StorageResult<"closestDescendantMerkleValue">
+      descendantsValues: StorageResult<"descendantsValues">
+      descendantsHashes: StorageResult<"descendantsHashes">
+    } = {
+      value: null,
+      hash: null,
+      closestDescendantMerkleValue: null,
+      descendantsValues: [],
+      descendantsHashes: [],
+    }
+    const onItems = (items: StorageItemResponse[]) => {
+      if (isDescendantsValues(type)) {
+        result[type].push(...(items as Array<{ key: string; value: string }>))
+      }
+      if (isDescendantsHashes(type)) {
+        result[type].push(...(items as Array<{ key: string; hash: string }>))
+      }
+      if (
+        isValue(type) ||
+        isHash(type) ||
+        isClosestDescendantMerkleValue(type)
+      ) {
+        result[type] = items[0]?.[type] ?? null
+      }
+    }
 
-      const onItems = isDescendants
-        ? (items: StorageItemResponse[]) => {
-            result.push(...items)
-          }
-        : (items: StorageItemResponse[]) => {
-            result = items[0]?.[type as "value"]
-          }
+    const cancel = cbStore(
+      hash,
+      [{ key, type }],
+      childTrie,
+      onItems,
+      reject,
+      () => resolve(result[type] as StorageResult<typeof type>),
+      (nDiscarded) => {
+        if (nDiscarded > 0) {
+          cancel()
+          reject(new OperationLimitError())
+        }
+      },
+    )
 
-      const cancel = cbStore(
-        hash,
-        [{ key, type }],
-        childTrie ?? null,
-        onItems,
-        reject,
-        () => {
-          resolve(result)
-        },
-        (nDiscarded) => {
-          if (nDiscarded > 0) {
-            cancel()
-            reject(new OperationLimitError())
-          }
-        },
-      )
-      return cancel
-    },
-  )
+    return cancel
+  })
+}
+
+function isValue<T extends StorageItemInput["type"]>(
+  type: T,
+): type is Extract<T, "value"> {
+  return type === "value"
+}
+
+function isHash<T extends StorageItemInput["type"]>(
+  type: T,
+): type is Extract<T, "hash"> {
+  return type === "hash"
+}
+
+function isClosestDescendantMerkleValue<T extends StorageItemInput["type"]>(
+  type: T,
+): type is Extract<T, "closestDescendantMerkleValue"> {
+  return type === "closestDescendantMerkleValue"
+}
+
+function isDescendantsValues<T extends StorageItemInput["type"]>(
+  type: T,
+): type is Extract<T, "descendantsValues"> {
+  return type === "descendantsValues"
+}
+
+function isDescendantsHashes<T extends StorageItemInput["type"]>(
+  type: T,
+): type is Extract<T, "descendantsHashes"> {
+  return type === "descendantsHashes"
 }
