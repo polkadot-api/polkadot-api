e5d0398372cfda5a945d1215da5121e7485127aa
Ryan Lee <ryan@parity.io>
2023-09-29 18:05:41 -0400

    fixes

--- packages/cli/src/io.ts
+++ packages/cli/src/io.ts
@@ -204,48 +204,46 @@ export async function outputCodegen(
 
   const variables = [...declarations.variables.values()]
 
-  const constDeclarations = await Promise.all(
-    variables.map(async (variable) => {
-      const value = variable.value.replace(/(\r\n|\n|\r)/gm, "")
-      const enumRegex = /(?<=Enum\(\{)(.*)(?=\}.*\))/g
-      const vectorRegex = /(?<=Vector\()([a-zA-Z0-9_]*)(?=.*\))/g
-      const tupleRegex = /(?<=Tuple\()(.*)(?=.*\))/g
+  const constDeclarations = variables.map((variable) => {
+    const value = variable.value.replace(/(\r\n|\n|\r)/gm, "")
+    const enumRegex = /(?<=Enum\(\{)(.*)(?=\}.*\))/g
+    const vectorRegex = /(?<=Vector\()([a-zA-Z0-9_]*)(?=.*\))/g
+    const tupleRegex = /(?<=Tuple\()(.*)(?=.*\))/g
 
-      const vectorMatch = value.match(vectorRegex)
-      const tupleMatch = value.match(tupleRegex)
-      const enumMatch = value.match(enumRegex)
+    const vectorMatch = value.match(vectorRegex)
+    const tupleMatch = value.match(tupleRegex)
+    const enumMatch = value.match(enumRegex)
 
-      let declaration = ""
+    let declaration = ""
 
-      if (vectorMatch && vectorMatch[0]) {
-        declaration += `type I${variable.id} = Codec<CodecType<typeof ${vectorMatch[0]}>[]>\n`
-        declaration += `const ${variable.id}: I${variable.id} = ${variable.value}\n`
-      } else if (tupleMatch && tupleMatch[0]) {
-        const tupleValues = tupleMatch[0].split(",").map((s) => s.trim())
-        declaration += `type I${variable.id} = Codec<[${tupleValues.map(
-          (s) => `CodecType<typeof ${s}>`,
-        )}]>\n`
-        declaration += `const ${variable.id}: I${variable.id} = ${variable.value}\n`
-      } else if (enumMatch && enumMatch[0]) {
-        const enumKeyValues = enumMatch[0]
-          .split(",")
-          .map((s) => s.split(":").map((s) => s.trim()) as [string, string])
+    if (vectorMatch && vectorMatch[0]) {
+      declaration += `type I${variable.id} = Codec<CodecType<typeof ${vectorMatch[0]}>[]>\n`
+      declaration += `const ${variable.id}: I${variable.id} = ${variable.value}\n`
+    } else if (tupleMatch && tupleMatch[0]) {
+      const tupleValues = tupleMatch[0].split(",").map((s) => s.trim())
+      declaration += `type I${variable.id} = Codec<[${tupleValues.map(
+        (s) => `CodecType<typeof ${s}>`,
+      )}]>\n`
+      declaration += `const ${variable.id}: I${variable.id} = ${variable.value}\n`
+    } else if (enumMatch && enumMatch[0]) {
+      const enumKeyValues = enumMatch[0]
+        .split(",")
+        .map((s) => s.split(":").map((s) => s.trim()) as [string, string])
+      declaration += `type I${variable.id} = Codec<${enumKeyValues
+        .map(([k, v]) => `\{tag: \"${k}\", value: CodecType<typeof ${v}> \}`)
+        .join("|")}>\n`
+      declaration += `const ${variable.id}: I${variable.id} = ${variable.value}\n`
+    } else {
+      declaration += `const ${variable.id}${
+        variable.types ? ": " + variable.types : ""
+      } = ${variable.value}\n`
+    }
 
-        declaration += `type I${variable.id} = Codec<${enumKeyValues
-          .map(([k, v]) => `\{tag: \"${k}\", value: CodecType<typeof ${v}> \}`)
-          .join("|")}>\n`
-        declaration += `const ${variable.id}: I${variable.id} = ${variable.value}\n`
-      } else {
-        declaration += `const ${variable.id}${
-          variable.types ? ": " + variable.types : ""
-        } = ${variable.value}\n`
-      }
+    declaration += `export type ${variable.id} = CodecType<typeof ${variable.id}>\n`
 
-      declaration += `export type ${variable.id} = CodecType<typeof ${variable.id}>\n`
+    return declaration
+  })
 
-      return declaration
-    }),
-  )
   constDeclarations.unshift(
     `import {${[...declarations.imports].join(
       ", ",
