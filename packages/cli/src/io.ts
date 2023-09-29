c596a9775e5130c3af13a26bfc3b3e2d1df04c45
Ryan Lee <ryan@parity.io>
2023-09-29 23:48:50 +0100

    fix(cli): codegen inference for large types (#107)

--- packages/cli/src/io.ts
+++ packages/cli/src/io.ts
@@ -195,14 +195,55 @@ export async function outputCodegen(
     }
   }
 
-  const constDeclarations = [...declarations.variables.values()].map(
-    (variable) =>
-      `const ${variable.id}${variable.types ? ": " + variable.types : ""} = ${
-        variable.value
-      }\nexport type ${variable.id} = CodecType<typeof ${variable.id}>`,
-  )
+  await fs.mkdir(outputFolder, { recursive: true })
+
   declarations.imports.add("CodecType")
   declarations.imports.add("Codec")
+  declarations.imports.add("SS58String")
+  declarations.imports.add("HexString")
+
+  const variables = [...declarations.variables.values()]
+
+  const constDeclarations = variables.map((variable) => {
+    const value = variable.value.replace(/(\r\n|\n|\r)/gm, "")
+    const enumRegex = /(?<=Enum\(\{)(.*)(?=\}.*\))/g
+    const vectorRegex = /(?<=Vector\()([a-zA-Z0-9_]*)(?=.*\))/g
+    const tupleRegex = /(?<=Tuple\()(.*)(?=.*\))/g
+
+    const vectorMatch = value.match(vectorRegex)
+    const tupleMatch = value.match(tupleRegex)
+    const enumMatch = value.match(enumRegex)
+
+    let declaration = ""
+
+    if (vectorMatch && vectorMatch[0]) {
+      declaration += `type I${variable.id} = Codec<CodecType<typeof ${vectorMatch[0]}>[]>\n`
+      declaration += `const ${variable.id}: I${variable.id} = ${variable.value}\n`
+    } else if (tupleMatch && tupleMatch[0]) {
+      const tupleValues = tupleMatch[0].split(",").map((s) => s.trim())
+      declaration += `type I${variable.id} = Codec<[${tupleValues.map(
+        (s) => `CodecType<typeof ${s}>`,
+      )}]>\n`
+      declaration += `const ${variable.id}: I${variable.id} = ${variable.value}\n`
+    } else if (enumMatch && enumMatch[0]) {
+      const enumKeyValues = enumMatch[0]
+        .split(",")
+        .map((s) => s.split(":").map((s) => s.trim()) as [string, string])
+      declaration += `type I${variable.id} = Codec<${enumKeyValues
+        .map(([k, v]) => `\{tag: \"${k}\", value: CodecType<typeof ${v}> \}`)
+        .join("|")}>\n`
+      declaration += `const ${variable.id}: I${variable.id} = ${variable.value}\n`
+    } else {
+      declaration += `const ${variable.id}${
+        variable.types ? ": " + variable.types : ""
+      } = ${variable.value}\n`
+    }
+
+    declaration += `export type ${variable.id} = CodecType<typeof ${variable.id}>\n`
+
+    return declaration
+  })
+
   constDeclarations.unshift(
     `import {${[...declarations.imports].join(
       ", ",
@@ -211,8 +252,6 @@ export async function outputCodegen(
 
   const tscFileName = path.join(outputFolder, key)
   const tscTypesFileName = path.join(outputFolder, `${key}-types`)
-
-  await fs.mkdir(outputFolder, { recursive: true })
 
   if (await fsExists(`${tscTypesFileName}.d.ts`)) {
     await fs.rm(`${tscTypesFileName}.d.ts`)
@@ -397,6 +436,6 @@ export type ${constName} = StorageType<typeof ${constName}>
     },
   })
 
-  const results = await eslint.lintFiles([`${outputFolder}/**/*.ts`])
+  const results = await eslint.lintFiles([`${outputFolder}/${key}.ts`])
   await ESLint.outputFixes(results)
 }
