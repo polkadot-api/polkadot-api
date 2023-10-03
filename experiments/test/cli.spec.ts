f24e2fe5d27d593aef2191e104666f299948927a
drd <drdgvhbh@gmail.com>
2023-10-03 13:04:05 +0200

    aaa

--- experiments/test/cli.spec.ts
+++ experiments/test/cli.spec.ts
@@ -6,10 +6,7 @@ describe("cli", () => {
   test(
     "descriptor codegen",
     async () => {
-      await runner()
-        .spawn("node_modules/.bin/polkadot-api", ["--key polkadot"], {})
-        .code(0)
-        .end()
+      await runner().spawn("polkadot-api", ["--key polkadot"], {}).code(0).end()
       await expect(
         fsExists("src/descriptors/polkadot-types.d.ts"),
       ).resolves.toEqual(true)
