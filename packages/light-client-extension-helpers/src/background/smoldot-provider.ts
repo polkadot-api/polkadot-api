902182b6523f024f7ed7d537ea1db0a18b0d70b1
Matias Volpe <matias@parity.io>
2023-10-03 13:37:28 -0300

    feat: remove cache from smoldotProvider

--- packages/light-client-extension-helpers/src/background/smoldot-provider.ts
+++ packages/light-client-extension-helpers/src/background/smoldot-provider.ts
@@ -1,7 +1,5 @@
 import type { GetProvider } from "@polkadot-api/json-rpc-provider"
-import type { Chain, Client } from "smoldot"
-
-const getProviderChains = new WeakMap<GetProvider, Chain>()
+import type { Client } from "smoldot"
 
 type SmoldotProviderOptions = {
   smoldotClient: Client
@@ -20,7 +18,7 @@ export const smoldotProvider = async ({
     //   potentialRelayChains: []
     databaseContent,
   })
-  const getProvider: GetProvider = (onMessage, onStatus) => {
+  return (onMessage, onStatus) => {
     let opened = false
     return {
       open() {
@@ -53,15 +51,10 @@ export const smoldotProvider = async ({
         } catch (error) {
           console.error("error removing chain", error)
         }
-        getProviderChains.delete(getProvider)
-        // TODO: validate, Should onStatus be invoked on .close()?
-        onStatus("disconnected")
       },
       send(msg: string) {
         chain.sendJsonRpc(msg)
       },
     }
   }
-  getProviderChains.set(getProvider, chain)
-  return getProvider
 }
