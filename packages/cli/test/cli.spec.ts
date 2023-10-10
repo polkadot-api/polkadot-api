e5364de7bb60b1252a16be8233e451ebfdd946d2
drd <drdgvhbh@gmail.com>
2023-10-10 12:19:53 +0200

    fixes

--- packages/cli/test/cli.spec.ts
+++ packages/cli/test/cli.spec.ts
@@ -1,24 +1,29 @@
 import { describe, test, expect } from "vitest"
 import { runner } from "clet"
 import fsExists from "fs.promises.exists"
+import path from "path"
 
 const cmd = "./bin/main.js"
+const outputDir = "artifacts/descriptors"
 
 describe("cli", () => {
   test(
     "descriptor codegen",
     async () => {
       await runner()
-        .file("package.json", { bin: { "polkadot-api": "./bin/main.js" } })
+        .file("package.json", { bin: { "polkadot-api": cmd } })
+        .file("test_descriptors.json", {
+          polkadot: { outputFolder: outputDir },
+        })
         .spawn(cmd, ["--file test_descriptors.json"], {})
         .code(0)
         .end()
       await expect(
-        fsExists("src/descriptors/polkadot-types.d.ts"),
+        fsExists(path.join(outputDir, "polkadot-types.d.ts")),
       ).resolves.toEqual(true)
-      await expect(fsExists("src/descriptors/polkadot.ts")).resolves.toEqual(
-        true,
-      )
+      await expect(
+        fsExists(path.join(outputDir, "polkadot.ts")),
+      ).resolves.toEqual(true)
     },
     5 * 60_000,
   )
