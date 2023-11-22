cfb3d67c6993ca16ff40a0d575f1900c4fb64d57
Matias <kratico@users.noreply.github.com>
2023-11-22 17:41:14 -0300

    feat(light-client-extension-helpers): add wait for persisted well known chains

--- packages/light-client-extension-helpers/src/background/background-helper.ts
+++ packages/light-client-extension-helpers/src/background/background-helper.ts
@@ -111,6 +111,7 @@ export const register = ({
     )
   })
 
+  const wellKnownChainsPersisted = deferred()
   chrome.runtime.onInstalled.addListener(({ reason }) => {
     if (
       reason !== chrome.runtime.OnInstalledReason.INSTALL &&
@@ -122,13 +123,15 @@ export const register = ({
       periodInMinutes: 2,
     })
 
-    wellKnownChainSpecsPromise.then((wellKnownChainSpecs) =>
-      Promise.all(
-        Object.values(wellKnownChainSpecs).map((chainSpec) =>
-          lightClientPageHelper.persistChain(chainSpec),
+    wellKnownChainSpecsPromise
+      .then((wellKnownChainSpecs) =>
+        Promise.all(
+          Object.values(wellKnownChainSpecs).map((chainSpec) =>
+            lightClientPageHelper.persistChain(chainSpec),
+          ),
         ),
-      ),
-    )
+      )
+      .then(() => wellKnownChainsPersisted.resolve())
   })
 
   const lightClientPageHelper: LightClientPageHelper = {
@@ -200,6 +203,7 @@ export const register = ({
       ])
     },
     async getChains() {
+      await wellKnownChainsPersisted
       const chains = await storage.getChains()
       return Promise.all(
         Object.entries(chains).map(async ([genesisHash, chain]) => ({
@@ -946,3 +950,52 @@ const createSmoldotProvider = async ({
 
 const createSmoldotSyncProvider = (options: CreateSmoldotProviderOptions) =>
   getSyncProvider(() => createSmoldotProvider(options))
+
+// Copyright 2018-2023 the Deno authors. All rights reserved. MIT license.
+// This module is browser compatible.
+
+// TODO(ry) It'd be better to make Deferred a class that inherits from
+// Promise, rather than an interface. This is possible in ES2016, however
+// typescript produces broken code when targeting ES5 code.
+// See https://github.com/Microsoft/TypeScript/issues/15202
+// At the time of writing, the github issue is closed but the problem remains.
+interface Deferred<T> extends Promise<T> {
+  readonly state: "pending" | "fulfilled" | "rejected"
+  resolve(value?: T | PromiseLike<T>): void
+  // deno-lint-ignore no-explicit-any
+  reject(reason?: any): void
+}
+
+/**
+ * Creates a Promise with the `reject` and `resolve` functions placed as methods
+ * on the promise object itself.
+ *
+ * @example
+ * ```typescript
+ * import { deferred } from "https://deno.land/std@$STD_VERSION/async/deferred.ts";
+ *
+ * const p = deferred<number>();
+ * // ...
+ * p.resolve(42);
+ * ```
+ */
+function deferred<T>(): Deferred<T> {
+  let methods
+  let state = "pending"
+  const promise = new Promise<T>((resolve, reject) => {
+    methods = {
+      async resolve(value: T | PromiseLike<T>) {
+        await value
+        state = "fulfilled"
+        resolve(value)
+      },
+      // deno-lint-ignore no-explicit-any
+      reject(reason?: any) {
+        state = "rejected"
+        reject(reason)
+      },
+    }
+  })
+  Object.defineProperty(promise, "state", { get: () => state })
+  return Object.assign(promise, methods) as Deferred<T>
+}
