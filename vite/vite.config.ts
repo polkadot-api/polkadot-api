ba20077b5762cd091c626f4b5a07804abd0787ca
Josep M Sobrepere <jm.sobrepere@gmail.com>
2023-12-11 13:17:25 +0100

    feat(client): support namespaces

--- vite/vite.config.ts
+++ vite/vite.config.ts
@@ -1,8 +1,6 @@
-import descriptorTreeShake from "@polkadot-api/rollup-plugin-descriptor-treeshake"
 import { defineConfig } from "vite"
 
 export default defineConfig({
-  plugins: [descriptorTreeShake("./src/codegen")],
   build: {
     target: "esnext",
     rollupOptions: {
