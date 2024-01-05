d364894e4d2a72aea265c37df9cb75b316022101
Matias Volpe <kratico@users.noreply.github.com>
2024-01-05 12:57:25 -0300

    chore: remove @polkadot-api/light-client-extension-helpers (#233)

--- examples/extension/src/protocol.ts
+++
@@ -1,7 +0,0 @@
-import type { InputChain } from "@polkadot-api/light-client-extension-helpers/background"
-
-export type ToContent = {
-  origin: "my-extension-background"
-  type: "onAddChainByUser"
-  inputChain: InputChain
-}
