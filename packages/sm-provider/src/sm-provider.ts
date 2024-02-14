f9af305b858d931f35b8b3493ab68a6ca98ba35e
Josep M Sobrepere <jm.sobrepere@gmail.com>
2024-02-15 00:55:58 +0100

    feat: add `@polkadot-api/sm-provider` (#268)

---
+++ packages/sm-provider/src/sm-provider.ts
@@ -0,0 +1,41 @@
+import type { AddChainOptions, Client } from "smoldot"
+import type { ConnectProvider } from "@polkadot-api/json-rpc-provider"
+import { getSyncProvider } from "@polkadot-api/json-rpc-provider-proxy"
+
+export const SmProvider =
+  (smoldot: Client) =>
+  (chainSpecOrOptions: string | AddChainOptions): ConnectProvider =>
+    getSyncProvider(async () => {
+      const chain = await smoldot.addChain(
+        typeof chainSpecOrOptions === "string"
+          ? { chainSpec: chainSpecOrOptions }
+          : chainSpecOrOptions,
+      )
+
+      return (listener, onError) => {
+        let listening = true
+        ;(async () => {
+          do {
+            let message = ""
+            try {
+              message = await chain.nextJsonRpcResponse()
+            } catch (e) {
+              if (listening) onError()
+              return
+            }
+            if (!listening) break
+            listener(message)
+          } while (listening)
+        })()
+
+        return {
+          send(msg: string) {
+            chain.sendJsonRpc(msg)
+          },
+          disconnect() {
+            listening = false
+            chain.remove()
+          },
+        }
+      }
+    })
