ad27c1edfc09c49e693e7487fe27e1a6e24f3575
Josep M Sobrepere <jm.sobrepere@gmail.com>
2024-03-08 10:05:22 +0100

    chore(experiments): update client experiment

--- experiments/src/client.ts
+++ experiments/src/client.ts
@@ -1,159 +1,20 @@
 import { getScProvider, WellKnownChain } from "@polkadot-api/sc-provider"
-import { noop } from "@polkadot-api/utils"
 import { getChain } from "@polkadot-api/node-polkadot-provider"
-import { createClient } from "@polkadot-api/client"
+import { createClient, Binary } from "@polkadot-api/client"
 
 // hint: remember to run the `codegen` script
-import Ksm, {
-  KsmXcmV2OriginKind,
-  KsmXcmV3Instruction,
-  KsmXcmV3JunctionJunction,
-  KsmXcmV3JunctionsJunctions,
-  KsmXcmV3MultiassetAssetId,
-  KsmXcmV3MultiassetAssetInstance,
-  KsmXcmV3MultiassetFungibility,
-  KsmXcmVersionedXcm,
-  Queries,
-} from "./descriptors/ksm"
-import { Enum, Binary, EnumOption } from "@polkadot-api/substrate-bindings"
+import Ksm, { Queries } from "./descriptors/ksm"
 const scProvider = getScProvider()
 
-const polkadotChain = await getChain({
+const polkadotChain = getChain({
   provider: scProvider(WellKnownChain.ksmcc3).relayChain,
-  keyring: { getPairs: () => [], onKeyPairsChanged: () => noop },
+  keyring: [],
 })
 
-const relayChain = createClient(polkadotChain.connect, { Ksm, Ksm1_2: Ksm })
-const collectives = relayChain
+const relayChain = createClient(polkadotChain)
+// const collectives = relayChain
 
-relayChain.Ksm.tx.XcmPallet.execute({
-  message: KsmXcmVersionedXcm.V3([
-    KsmXcmV3Instruction.LockAsset({
-      asset: {
-        id: KsmXcmV3MultiassetAssetId.Abstract(Binary.fromText("")),
-        fun: KsmXcmV3MultiassetFungibility.Fungible(4n),
-      },
-      unlocker: {
-        parents: 4,
-        interior: KsmXcmV3JunctionsJunctions.X1(
-          KsmXcmV3JunctionJunction.Parachain(4),
-        ),
-      },
-    }),
-  ]),
-  max_weight: {
-    ref_time: 3n,
-    proof_size: 5n,
-  },
-})
-
-const transact1: EnumOption<KsmXcmV3Instruction, "Transact"> = {
-  call: Binary.fromText(""),
-  origin_kind: Enum("Xcm"),
-  require_weight_at_most: {
-    ref_time: 3n,
-    proof_size: 33n,
-  },
-}
-
-const instructions: Array<KsmXcmV3Instruction> = [
-  Enum("Transact", transact1),
-  Enum("WithdrawAsset", [
-    {
-      id: Enum("Concrete", {
-        parents: 3,
-        interior: Enum("Here"),
-      }),
-      fun: Enum("Fungible", 5n),
-    },
-  ]),
-]
-
-relayChain.Ksm.tx.XcmPallet.execute({
-  message: Enum("V3", instructions),
-  max_weight: {
-    proof_size: 3n,
-    ref_time: 3n,
-  },
-})
-
-relayChain.Ksm.tx.XcmPallet.execute({
-  message: KsmXcmVersionedXcm.V3([]),
-  max_weight: {
-    ref_time: 5n,
-    proof_size: 3n,
-  },
-})
-relayChain.Ksm.tx.XcmPallet.execute({
-  message: KsmXcmVersionedXcm.V3([
-    KsmXcmV3Instruction.ExpectOrigin({
-      parents: 3,
-      interior: Enum("X1", Enum("Parachain", 3)),
-    }),
-  ]),
-  max_weight: {
-    ref_time: 5n,
-    proof_size: 33n,
-  },
-})
-
-relayChain.Ksm.tx.XcmPallet.execute({
-  message: KsmXcmVersionedXcm.V3([
-    KsmXcmV3Instruction.Transact({
-      call: Binary.fromHex("0x32ff"),
-      origin_kind: KsmXcmV2OriginKind.Native(),
-      require_weight_at_most: {
-        ref_time: 5n,
-        proof_size: 3n,
-      },
-    }),
-    KsmXcmV3Instruction.BurnAsset([
-      {
-        id: KsmXcmV3MultiassetAssetId.Abstract(Binary.fromText("")),
-        fun: KsmXcmV3MultiassetFungibility.NonFungible(
-          KsmXcmV3MultiassetAssetInstance.Index(3n),
-        ),
-      },
-    ]),
-  ]),
-  max_weight: { ref_time: 3n, proof_size: 3n },
-})
-
-relayChain.Ksm.tx.Utility.batch({
-  calls: [
-    Enum("Identity", Enum("set_fee", { index: 2, fee: 3n })),
-    Enum(
-      "Beefy",
-      Enum("report_equivocation_unsigned", {
-        equivocation_proof: {
-          first: {
-            commitment: {
-              payload: [[Binary.fromText(""), Binary.fromText("")]],
-              block_number: 3,
-              validator_set_id: 3n,
-            },
-            id: Binary.fromText(""),
-            signature: Binary.fromText(""),
-          },
-          second: {
-            commitment: {
-              payload: [[Binary.fromText(""), Binary.fromText("")]],
-              block_number: 3,
-              validator_set_id: 3n,
-            },
-            id: Binary.fromText(""),
-            signature: Binary.fromText(""),
-          },
-        },
-        key_owner_proof: {
-          session: 3,
-          trie_nodes: [Binary.fromText("")],
-          validator_count: 3,
-        },
-      }),
-    ),
-  ],
-})
+const apis = relayChain.getTypedApi(Ksm)
 
 const identityDataToString = (value: string | Binary | undefined) =>
   typeof value === "object" ? value.asText() : value ?? ""
@@ -189,10 +50,10 @@ function mapRawIdentity(
 }
 
 const relevantIdentities =
-  await collectives.Ksm.query.FellowshipCollective.Members.getEntries()
+  await apis.query.FellowshipCollective.Members.getEntries()
     .then((allMembers) => allMembers.filter(({ value }) => value >= 4))
     .then((members) =>
-      relayChain.Ksm.query.Identity.IdentityOf.getValues(
+      apis.query.Identity.IdentityOf.getValues(
         members.map((m) => m.keyArgs),
       ).then((identities) =>
         identities.map((identity, idx) => ({
@@ -204,12 +65,3 @@ const relevantIdentities =
     )
 
 relevantIdentities.forEach((identity) => console.log(identity))
-
-const runtime = await relayChain.runtime.latest()
-
-console.log(runtime.constants.Ksm.System.Version.spec_name)
-console.log(runtime.constants.Ksm.System.Version.spec_version)
-console.log(
-  "Is Balances.transfer_allow_death compatible:",
-  runtime.isCompatible((api) => api.Ksm.tx.Balances.transfer_allow_death),
-)
