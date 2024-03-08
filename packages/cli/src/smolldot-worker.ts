89565d09c8d1dafe12c34ecf0ab5dfbf94078847
Victor Oliva <v.oliva@proton.me>
2024-03-08 09:52:29 +0100

    fix(cli): parachain chainspec support

--- packages/cli/src/smolldot-worker.ts
+++ packages/cli/src/smolldot-worker.ts
@@ -1,19 +1,34 @@
 export const PROVIDER_WORKER_CODE = `
 const { parentPort, workerData } = require("node:worker_threads")
 const { getScProvider } = require("@polkadot-api/sc-provider")
-
-const chain = workerData
-const scProvider = getScProvider()
-const getProvider = scProvider(chain).relayChain
+const { WellKnownChain } = require("@substrate/connect")
+const wellKnownChains = new Set(Object.values(WellKnownChain))
+const isWellKnownChain = (input) => wellKnownChains.has(input)
 
 if (!parentPort) {
   throw new Error("no parent port")
 }
 
-const provider = getProvider(
-  (msg) => parentPort.postMessage(msg)
-)
+const chain = workerData
+const scProvider = getScProvider()
+const getProvider = isWellKnownChain(chain)
+  ? scProvider(chain).relayChain
+  : (() => {
+      const parsedSpec = JSON.parse(chain)
+      if (parsedSpec.relay_chain) {
+        if (!isWellKnownChain(parsedSpec.relay_chain)) {
+          throw new Error(
+            "Relay chain " + parsedSpec.relay_chain + " is not well-known",
+          )
+        }
+        const provider = scProvider(parsedSpec.relay_chain)
+        return provider.addParachain(chain)
+      } else {
+        return scProvider(chain).relayChain
+      }
+    })()
 
+const provider = getProvider((msg) => parentPort.postMessage(msg))
 parentPort.on("message", (msg) => {
   switch (msg.type) {
     case "send":
