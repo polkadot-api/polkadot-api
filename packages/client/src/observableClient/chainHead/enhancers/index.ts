f35bddb3b4c03804cc858b5dd33ad847f97e2321
Josep M Sobrepere <jm.sobrepere@gmail.com>
2024-03-17 15:59:01 +0100

    chore(client): remove dead code

--- packages/client/src/observableClient/chainHead/enhancers/index.ts
+++ packages/client/src/observableClient/chainHead/enhancers/index.ts
@@ -1,5 +1,4 @@
 export { getWithRecovery } from "./operationLimitRecovery"
-export { getWithUnpinning$ } from "./unpin"
 export { getWithOptionalhash$ } from "./optionalHash"
 export { fromAbortControllerFn } from "./fromAbortControllerFn"
 export { withLazyFollower } from "./lazyFollower"
