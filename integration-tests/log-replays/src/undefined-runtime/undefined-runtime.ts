505e85ee4e2bb55f5319af9eab19122de6e0deb0
Josep M Sobrepere <jm.sobrepere@gmail.com>
2024-03-28 16:52:43 +0100

    tests: add integration test for undefined-runtime bug

---
+++ integration-tests/log-replays/src/undefined-runtime/undefined-runtime.ts
@@ -0,0 +1,38 @@
+import { createClient } from "@polkadot-api/client"
+import { getChain } from "@polkadot-api/node-polkadot-provider"
+import roc from "../codegen/roc"
+import { logsProvider } from "@polkadot-api/logs-provider"
+import { readFile } from "fs/promises"
+
+const LOG_FILE = "./src/undefined-runtime/logs.txt"
+
+const logs = (await readFile(LOG_FILE, { encoding: "utf-8" })).split("\n")
+
+const client = createClient(
+  getChain({
+    provider: logsProvider(logs),
+    keyring: [],
+  }),
+)
+
+const api = client.getTypedApi(roc)
+
+api.query.System.Account.getValue(
+  "5H1Dyus4jYJSCffxw7k7eTvV3v69CD7cyw2f7qBY2FJH1Gev",
+  { at: "finalized" },
+).then((value) => {
+  console.log(
+    "I got an update on 5H1Dyus4jYJSCffxw7k7eTvV3v69CD7cyw2f7qBY2FJH1Gev",
+    value,
+  )
+})
+
+api.query.System.Account.watchValue(
+  "5GbYjtbenzxE2YpV7tkZhdeHZs3M5h5HR8es2c2a2W3RUnpx",
+  "finalized",
+).forEach((value) => {
+  console.log(
+    "I got an update on 5GbYjtbenzxE2YpV7tkZhdeHZs3M5h5HR8es2c2a2W3RUnpx",
+    value,
+  )
+})
