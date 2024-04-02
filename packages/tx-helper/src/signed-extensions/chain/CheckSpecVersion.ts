68113d12133d0e6ee098451e124f87c7b832304a
Josep M Sobrepere <jm.sobrepere@gmail.com>
2024-04-02 23:38:07 +0200

    remove `@polkadot-api/tx-helpers`

--- packages/tx-helper/src/signed-extensions/chain/CheckSpecVersion.ts
+++
@@ -1,7 +0,0 @@
-import type { GetChainSignedExtension } from "@/internal-types"
-import { empty$, systemVersionProp$ } from "../utils"
-
-export const CheckSpecVersion: GetChainSignedExtension = ({ metadata }) => ({
-  additionalSigned: systemVersionProp$("spec_version", metadata),
-  value: empty$,
-})
--- packages/tx-helper/src/signed-extensions/chain/CheckTxVersion.ts
+++
@@ -1,7 +0,0 @@
-import type { GetChainSignedExtension } from "@/internal-types"
-import { empty$, systemVersionProp$ } from "../utils"
-
-export const CheckTxVersion: GetChainSignedExtension = ({ metadata }) => ({
-  additionalSigned: systemVersionProp$("transaction_version", metadata),
-  value: empty$,
-})
