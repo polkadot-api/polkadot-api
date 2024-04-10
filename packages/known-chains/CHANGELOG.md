65331d468256b0695a489bf69a564aac4f2fb3ad
Victor Oliva <v.oliva@proton.me>
2024-04-10 13:02:11 +0200

    feat: create @polkadot-api/known-chains package

---
+++ packages/known-chains/CHANGELOG.md
@@ -0,0 +1,9 @@
+# Changelog
+
+## Unreleased
+
+## 0.0.1 - 2024-04-10
+
+### Changed
+
+Initial release
---
+++ packages/known-chains/prepare-specs.mjs
@@ -0,0 +1,17 @@
+import { mkdir, readFile, readdir, writeFile } from "node:fs/promises"
+import { join } from "node:path"
+
+const specFiles = await readdir("specs")
+await mkdir(join("src", "specs"), { recursive: true })
+
+for (const filename of specFiles) {
+  const packageName = filename.replace(".json", "")
+  const fileContent = await readFile(join("specs", filename), "utf-8")
+
+  await writeFile(
+    join("src", "specs", `${packageName}.ts`),
+    `const ${packageName}: string = \`${fileContent}\`
+export default ${packageName}
+`,
+  )
+}
