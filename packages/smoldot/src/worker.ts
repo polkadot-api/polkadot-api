bb79f341d6b078f1773dea3e88ccebb41e767221
Victor Oliva <v.oliva@proton.me>
2024-04-11 09:39:36 +0200

    remove console.log

--- packages/smoldot/src/worker.ts
+++ packages/smoldot/src/worker.ts
@@ -2,7 +2,6 @@ import * as smoldot from "smoldot/worker"
 import { compileBytecode } from "smoldot/bytecode"
 
 compileBytecode().then((x) => {
-  console.log("compiled code", x)
   postMessage(x)
 })
 onmessage = (msg) => smoldot.run(msg.data)
