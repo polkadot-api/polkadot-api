df77770cdea190074ccf2682ed155f7d8e38cc9b
Josep M Sobrepere <jm.sobrepere@gmail.com>
2024-04-14 18:01:41 +0200

    feat(client): add `getEstimatedFees` API

--- packages/client/src/index.ts
+++ packages/client/src/index.ts
@@ -3,4 +3,9 @@ export * from "./re-exports"
 export type { EventPhase } from "./event"
 export type { PolkadotSigner } from "@polkadot-api/polkadot-signer"
 export type { PolkadotClient, TypedApi, FixedSizeArray } from "./types"
-export type { TxBroadcastEvent, TxEvent, TxFinalizedPayload } from "./tx"
+export type {
+  TxBroadcastEvent,
+  TxEvent,
+  TxFinalizedPayload,
+  Transaction,
+} from "./tx"
