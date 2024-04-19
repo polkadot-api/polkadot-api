20f994bba767610a84d1eb0e998669fdc8b625af
Victor Oliva <v.oliva@proton.me>
2024-04-19 18:10:33 +0200

    build: speed up client package build

--- packages/client/build.sh
+++ packages/client/build.sh
@@ -1,18 +1,11 @@
 #!/bin/bash
 set -e
 
-# Regular client
 pnpm tsc --noEmit
-pnpm tsup-node src/index.ts --clean --sourcemap --platform neutral --target=es2020 --format esm,cjs --dts
-pnpm tsup-node src/index.ts --clean --sourcemap --platform neutral --target=es2020 --format cjs --dts --minify --out-dir dist/min
 
-# CLI
+reexports=$(ls -1 ./src/reexports/*.ts | sed 's/\.\///g' | tr "\n" " ")
+
+pnpm tsup-node src/index.ts ${reexports} --clean --sourcemap --platform neutral --target=es2020 --format esm,cjs --dts
+pnpm tsup-node src/index.ts ${reexports} --clean --sourcemap --platform neutral --target=es2020 --format cjs --dts --minify --out-dir dist/min
+
 pnpm tsup-node src/cli.ts --clean --sourcemap --platform neutral --target=es2020 --format esm --out-dir "bin"
-
-# Re-exports
-for src in ./src/reexports/*.ts
-do
-    name=$(basename ${src%.ts})
-    pnpm tsup-node "${src}" --clean --sourcemap --platform neutral --target=es2020 --format esm,cjs --dts --out-dir "dist/${name}"
-    pnpm tsup-node "${src}" --clean --sourcemap --platform neutral --target=es2020 --format cjs --dts --minify --out-dir "dist/${name}/min"
-done
