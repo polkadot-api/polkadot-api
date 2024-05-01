91cd4617bb8f3cee279cda646cadcce3f76f253a
Victor Oliva <v.oliva@proton.me>
2024-05-01 12:24:57 +0200

    feat: redo known-types

--- examples/react-teleport/src/api/teleport.ts
+++ examples/react-teleport/src/api/teleport.ts
@@ -1,10 +1,10 @@
 import { SS58String, Binary, AccountId } from "polkadot-api"
 import {
-  XcmV4Junctions,
+  XcmV3Junctions,
   XcmV3MultiassetAssetId,
   XcmV3MultiassetFungibility,
   XcmV3WeightLimit,
-  XcmV4Junction,
+  XcmV3Junction,
   XcmVersionedAssets,
   XcmVersionedLocation,
 } from "@polkadot-api/descriptors"
@@ -17,8 +17,8 @@ const encodeAccount = AccountId().enc
 const getBeneficiary = (address: SS58String) =>
   XcmVersionedLocation.V3({
     parents: 0,
-    interior: XcmV4Junctions.X1(
-      XcmV4Junction.AccountId32({
+    interior: XcmV3Junctions.X1(
+      XcmV3Junction.AccountId32({
         network: undefined,
         id: Binary.fromBytes(encodeAccount(address)),
       }),
@@ -30,7 +30,7 @@ const getNativeAsset = (amount: bigint, parents: 1 | 0) =>
     {
       id: XcmV3MultiassetAssetId.Concrete({
         parents,
-        interior: XcmV4Junctions.Here(),
+        interior: XcmV3Junctions.Here(),
       }),
       fun: XcmV3MultiassetFungibility.Fungible(amount),
     },
@@ -40,7 +40,7 @@ export const teleportToParaChain = (address: SS58String, amount: bigint) =>
   relayChainApi.tx.XcmPallet.limited_teleport_assets({
     dest: XcmVersionedLocation.V3({
       parents: 0,
-      interior: XcmV4Junctions.X1(XcmV4Junction.Parachain(PARACHAIN_ID)),
+      interior: XcmV3Junctions.X1(XcmV3Junction.Parachain(PARACHAIN_ID)),
     }),
     beneficiary: getBeneficiary(address),
     assets: getNativeAsset(amount, 0),
@@ -52,7 +52,7 @@ export const teleportToRelayChain = (address: SS58String, amount: bigint) =>
   paraChainApi.tx.PolkadotXcm.limited_teleport_assets({
     dest: XcmVersionedLocation.V3({
       parents: 1,
-      interior: XcmV4Junctions.Here(),
+      interior: XcmV3Junctions.Here(),
     }),
     beneficiary: getBeneficiary(address),
     assets: getNativeAsset(amount, 1),
