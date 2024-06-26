4862604d6441880d3071277f849530443071f12b
Josep M Sobrepere <jm.sobrepere@gmail.com>
2024-06-26 12:20:30 +0200

    nit(metadata-hash): refined version (#542)
    Co-authored-by: Carlo Sala <carlosalag@protonmail.com>

--- packages/metadata-hash/tests/metadata-hash.spec.ts
+++ packages/metadata-hash/tests/metadata-hash.spec.ts
@@ -1,15 +1,22 @@
 import { buildMetadataHash } from "@/metadata-hash"
 import { getKsmMetadata } from "@polkadot-api/metadata-fixtures"
+import { metadata } from "@polkadot-api/substrate-bindings"
 import { beforeAll, describe, expect, it } from "vitest"
 
-let ksm: Awaited<ReturnType<typeof getKsmMetadata>>
+let ksmMetadata: Awaited<Uint8Array>
 beforeAll(async () => {
-  ksm = await getKsmMetadata()
+  ksmMetadata = metadata.enc({
+    magicNumber: 0x6174656d,
+    metadata: {
+      tag: "v15",
+      value: await getKsmMetadata(),
+    },
+  })
 })
 
 describe("getChecksumBuilder snapshots", () => {
   it("generates metadata hash", () => {
-    const result = buildMetadataHash(ksm, {
+    const result = buildMetadataHash(ksmMetadata, {
       decimals: 1,
       specName: "nice",
       base58Prefix: 1,
