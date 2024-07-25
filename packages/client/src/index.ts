3c0a36c35e89d784ee244ee82d67d3af21129bf9
Victor Oliva <v.oliva@proton.me>
2024-07-25 15:13:52 +0200

    specify types to export

--- packages/client/src/index.ts
+++ packages/client/src/index.ts
@@ -2,7 +2,25 @@ export * from "./client"
 export * from "./re-exports"
 export * from "./descriptors"
 export { InvalidTxError } from "./tx"
-export type * from "./tx/types"
+export type {
+  TxEvent,
+  TxBroadcastEvent,
+  TxSigned,
+  TxBroadcasted,
+  TxBestBlocksState,
+  TxInBestBlocksNotFound,
+  TxInBestBlocksFound,
+  TxEventsPayload,
+  TxFinalized,
+  TxOptions,
+  TxFinalizedPayload,
+  TxPromise,
+  TxObservable,
+  TxCall,
+  TxSignFn,
+  Transaction,
+  TxEntry,
+} from "./tx"
 export type { EventPhase } from "./event"
 export type { PolkadotSigner } from "@polkadot-api/polkadot-signer"
 export type {
