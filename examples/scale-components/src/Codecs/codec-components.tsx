8dd0feddd99a1f1d0d15e33835000bb153fe337e
Hannah Redler <hannahjredler@gmail.com>
2024-09-20 12:12:11 +0200

    Styling

--- examples/scale-components/src/Codecs/codec-components.tsx
+++ examples/scale-components/src/Codecs/codec-components.tsx
@@ -1,6 +1,9 @@
 import { toHex } from "@polkadot-api/utils"
 import { HexString, SS58String } from "@polkadot-api/substrate-bindings"
 import React, { createContext, ReactNode, useContext } from "react"
+import clsx from "clsx"
+import * as Accordion from "@radix-ui/react-accordion"
+import { ChevronDownIcon } from "@radix-ui/react-icons"
 
 export type CodecComponentProps<T = any> = {
   value: T
@@ -42,16 +45,28 @@ const withDepth: <T extends {}>(
 
 const enhancer = withDepth
 
-export const CNumber: React.FC<NumberInterface> = ({ type, value }) => (
-  <span>
-    {type} - {value}
-  </span>
+export const CNumber: React.FC<NumberInterface> = ({
+  type,
+  value,
+  encodedValue,
+}) => (
+  <PrimitiveDisplay
+    type={type.toString()}
+    value={value.toString()}
+    encodedValue={toHex(encodedValue).toString()}
+  />
 )
 
-export const CBNumber: React.FC<BNumberInterface> = ({ type, value }) => (
-  <span>
-    {type} - {value.toString(10)}n
-  </span>
+export const CBNumber: React.FC<BNumberInterface> = ({
+  type,
+  value,
+  encodedValue,
+}) => (
+  <PrimitiveDisplay
+    type={type.toString()}
+    value={`${value.toString(10)}n`}
+    encodedValue={toHex(encodedValue).toString()}
+  />
 )
 export const CBool: React.FC<BoolInterface> = ({ value }) => (
   <input type="checkbox" checked={value} />
@@ -60,15 +75,22 @@ export const CBool: React.FC<BoolInterface> = ({ value }) => (
 export const CVoid: React.FC<VoidInterface> = () => null
 
 export const CString: React.FC<StrInterface> = ({ value }) => (
-  <span>{value}n</span>
+  <span>{value}</span>
 )
 
 export const CBytes: React.FC<BytesInterface> = ({ value }) => (
   <span>{toHex(value)}</span>
 )
 
-export const CAccountId: React.FC<AccountIdInterface> = ({ value }) => (
-  <span>{value}</span>
+export const CAccountId: React.FC<AccountIdInterface> = ({
+  value,
+  encodedValue,
+}) => (
+  <PrimitiveDisplay
+    type="AccountId"
+    value={value.toString()}
+    encodedValue={toHex(encodedValue).toString()}
+  />
 )
 
 export const CEthAccount: React.FC<EthAccountInterface> = ({ value }) => (
@@ -87,7 +109,26 @@ export const CSequence: React.FC<SequenceInterface> = ({
 }) => {
   return (
     <>
-      <ul>{innerComponents}</ul>
+      <ul>
+        {innerComponents.map((component, idx) => {
+          return (
+            <Accordion.Root
+              type="single"
+              collapsible
+              className="bg-gray-800 rounded my-2 p-2"
+            >
+              <Accordion.AccordionItem value={`item-${idx}`}>
+                <Accordion.AccordionTrigger className="flex flex-row justify-between w-full items-center">
+                  ITEM {idx + 1} <ChevronDownIcon />
+                </Accordion.AccordionTrigger>
+                <Accordion.AccordionContent>
+                  {component}
+                </Accordion.AccordionContent>
+              </Accordion.AccordionItem>
+            </Accordion.Root>
+          )
+        })}
+      </ul>
       <span>{toHex(encodedValue)}</span>
     </>
   )
@@ -103,7 +144,16 @@ export const CArray: React.FC<ArrayInterface> = ({
 }) => {
   return (
     <>
-      <ul>{innerComponents}</ul>
+      <ul>
+        {innerComponents.map((component, idx) => {
+          return (
+            <li key={idx} className="bg-gray-700 rounded my-2 p-2">
+              <h3 className="font-bold mb-2">ITEM {idx + 1}</h3>
+              {component}
+            </li>
+          )
+        })}
+      </ul>
       <span>{toHex(encodedValue)}</span>
     </>
   )
@@ -127,16 +177,21 @@ export const CTuple: React.FC<TupleInterface> = ({ innerComponents }) => {
 export type StructInterface = CodecComponentProps<Record<string, any>> & {
   innerComponents: Record<string, React.ReactNode>
 }
-export const CStruct: React.FC<StructInterface> = ({ innerComponents }) => (
-  <ul>
-    {"{"}
-    {Object.entries(innerComponents).map(([key, jsx]) => (
-      <li>
-        {key}: {jsx},
-      </li>
-    ))}
-    {"}"}
-  </ul>
+export const CStruct: React.FC<StructInterface> = withDepth(
+  ({ innerComponents, depth }) => (
+    <div className="flex flex-col text-left">
+      <ul className="ml-[20px]">
+        {Object.entries(innerComponents).map(([key, jsx]) => (
+          <li key={key} className="flex flex-col mb-2">
+            <span className="font-semibold text-sm mb-2 text-gray-400">
+              {key}
+            </span>
+            {jsx}
+          </li>
+        ))}
+      </ul>
+    </div>
+  ),
 )
 
 type OptionInterface<T = any> = CodecComponentProps<T | undefined> & {
@@ -151,7 +206,7 @@ type ResultInterface = CodecComponentProps<{ success: boolean; value: any }> & {
   inner: ReactNode
 }
 
-export const CREsult: React.FC<ResultInterface> = ({ value, inner }) => (
+export const CResult: React.FC<ResultInterface> = ({ value, inner }) => (
   <>
     {value.success ? "ok" : "ko"}-{inner}
   </>
@@ -166,17 +221,36 @@ type EnumInterface = CodecComponentProps<{ type: string; value: any }> & {
 export const CEnum: React.FC<EnumInterface> = withDepth(
   ({ encodedValue, value, tags, onChange, inner, depth }) => {
     return (
-      <>
-        <span>{toHex(encodedValue)}</span>
-        <select onChange={(e) => onChange(e.target.value)}>
-          {tags.map(({ tag }) => (
-            <option key={tag} value={tag} selected={tag === value.type}>
-              {tag}
-            </option>
-          ))}
-        </select>
-        {inner}
-      </>
+      <div className={clsx("flex flex-col text-left gap-2 w-fit")}>
+        <div className="flex flex-row gap-5">
+          <select
+            className="w-fit bg-slate-700 p-2 rounded"
+            onChange={(e) => onChange(e.target.value)}
+          >
+            {tags.map(({ tag }) => (
+              <option key={tag} value={tag} selected={tag === value.type}>
+                {tag}
+              </option>
+            ))}
+          </select>
+          <span>{toHex(encodedValue)}</span>
+        </div>
+        <div style={{ marginLeft: "30px" }}>{inner}</div>
+      </div>
     )
   },
+)
+
+const PrimitiveDisplay: React.FC<{
+  type: string
+  value: string
+  encodedValue: string
+}> = ({ type, value, encodedValue }) => (
+  <div className="flex flex-row gap-5">
+    <span className="flex flex-row bg-gray-900 rounded p-2 w-fit min-w-20 gap-2 justify-between items-center px-2">
+      <span className="text-sm text-gray-500">{type}</span>
+      <span>{value}</span>
+    </span>
+    {encodedValue}
+  </div>
 )
