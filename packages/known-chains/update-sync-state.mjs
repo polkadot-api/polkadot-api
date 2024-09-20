07fc05c80d5e8d87eccf4649ab4c0a681127e3b0
Carlo Sala <carlosalag@protonmail.com>
2024-09-20 16:19:06 +0200

    chore(known-chains): add `update-sync-state.mjs` dev script

---
+++ packages/known-chains/update-sync-state.mjs
@@ -0,0 +1,31 @@
+import { createClient } from "@polkadot-api/substrate-client"
+import { getWsProvider } from "@polkadot-api/ws-provider/node"
+import { readFile, writeFile } from "node:fs/promises"
+
+const CHAINS = {
+  polkadot: "wss://polkadot-rpc.dwellir.com",
+  ksmcc3: "wss://rpc-kusama.luckyfriday.io",
+  westend2: "wss://westend-rpc.polkadot.io",
+  rococo_v2_2: "wss://rococo-rpc.polkadot.io",
+  paseo: "wss://paseo.rpc.amforc.com",
+}
+
+const main = async () => {
+  Object.entries(CHAINS).forEach(async ([name, rpc]) => {
+    try {
+      const filename = `./specs/${name}.json`
+      const client = createClient(getWsProvider(rpc))
+      const [{ lightSyncState }, spec] = await Promise.all([
+        client.request("sync_state_genSyncSpec", [true]),
+        readFile(filename, "utf8").then((v) => JSON.parse(v)),
+      ])
+      spec.lightSyncState = lightSyncState
+      await writeFile(filename, JSON.stringify(spec, null, 2) + "\n")
+      client.destroy()
+      console.log(`${name} done`)
+    } catch {
+      console.warn(`${name} DIDN'T WORK`)
+    }
+  })
+}
+main()
