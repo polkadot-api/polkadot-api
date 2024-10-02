de599e53bea4688e6381ea1c6e73915158f27e67
Josep M Sobrepere <jm.sobrepere@gmail.com>
2024-10-02 10:24:25 +0200

    feat(ws-provider): new APIs and improvements

---
+++ examples/bun/src/switch-rpcs.ts
@@ -0,0 +1,35 @@
+import { CompatibilityLevel, createClient } from "polkadot-api"
+import { getWsProvider } from "polkadot-api/ws-provider/web"
+import { wnd } from "@polkadot-api/descriptors"
+
+const wsProvider = getWsProvider(
+  [
+    "wss://polkadot-rpc.publicnode.com",
+    "wss://polkadot-public-rpc.blockops.network/ws",
+    "wss://rpc.ibp.network/polkadot",
+    "wss://rpc-polkadot.luckyfriday.io",
+    "wss://polkadot.api.onfinality.io/public-ws",
+  ],
+  (x) => {
+    console.log(x)
+  },
+)
+const client = createClient(wsProvider)
+const testApi = client.getTypedApi(wnd)
+
+client.finalizedBlock$.subscribe(console.log, console.error)
+testApi.query.System.Account.watchValue(
+  "5GvW9jJnq5J7KYAtYfdi88zXxVT3Z6dHHr6cgBgMpkVhPVrp",
+).subscribe(console.log, console.error)
+
+console.log("sync switch")
+wsProvider.switch()
+
+const switchWs = async () => {
+  await new Promise((res) => setTimeout(res, 12_000))
+  console.log("switching")
+  wsProvider.switch()
+  switchWs()
+}
+
+switchWs()
