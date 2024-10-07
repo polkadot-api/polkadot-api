fecf55ff7d7d22eec861b3c202c7cc0396eda5d4
Josep M Sobrepere <jm.sobrepere@gmail.com>
2024-10-07 10:40:46 +0200

    use `decAnyMetadata` from `substrate-bindings`

--- packages/tx-utils/src/get-metadata.ts
+++ packages/tx-utils/src/get-metadata.ts
@@ -1,44 +1,9 @@
-import {
-  Option,
-  Bytes,
-  metadata,
-  compact,
-  Tuple,
-  V15,
-  V14,
-} from "@polkadot-api/substrate-bindings"
+import { V15, V14, decAnyMetadata } from "@polkadot-api/substrate-bindings"
 import { HexString } from "@polkadot-api/substrate-bindings"
-
-const opaqueBytes = Bytes()
-const optionOpaque = Option(opaqueBytes)
-const opaqueOpaqueBytes = Tuple(compact, opaqueBytes)
-
-const getAnyMetadata = (input: Uint8Array | HexString) => {
-  try {
-    return metadata.dec(input)
-  } catch (_) {}
-
-  // comes from metadata.metadata_at_version
-  try {
-    return metadata.dec(optionOpaque.dec(input)!)
-  } catch (_) {}
-
-  // comes from state.getMetadata
-  try {
-    return metadata.dec(opaqueBytes.dec(input))
-  } catch (_) {}
-
-  // comes from metadata.metadata
-  try {
-    return metadata.dec(opaqueOpaqueBytes.dec(input)[1])
-  } catch (_) {}
-
-  throw null
-}
 
 export const getMetadata = (input: Uint8Array | HexString): V14 | V15 => {
   try {
-    const { metadata } = getAnyMetadata(input)
+    const { metadata } = decAnyMetadata(input)
     if (metadata.tag !== "v14" && metadata.tag !== "v15")
       throw new Error("Wrong metadata version")
     return metadata.value
