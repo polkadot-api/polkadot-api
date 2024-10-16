2b86fa1bfcbc39ddccdc1b17d88a433bce8971ac
Victor Oliva <v.oliva@proton.me>
2024-10-16 13:11:51 +0200

    feat(ink-client): nested storage (#775)

--- packages/ink-contracts/src/dynamic-builders.ts
+++ packages/ink-contracts/src/dynamic-builders.ts
@@ -1,87 +1,41 @@
 import { getLookupCodecBuilder } from "@polkadot-api/metadata-builders"
 import { Binary, Variant } from "@polkadot-api/substrate-bindings"
 import {
+  _void,
   Bytes,
   Codec,
   CodecType,
   enhanceCodec,
-  Enum,
-  Option,
   StringRecord,
   Struct,
   Tuple,
-  Vector,
 } from "scale-ts"
 import { InkMetadataLookup } from "./get-lookup"
-import { Layout, MessageParamSpec, TypeSpec } from "./metadata-types"
+import { MessageParamSpec, TypeSpec } from "./metadata-types"
 
 export const getInkDynamicBuilder = (metadataLookup: InkMetadataLookup) => {
   const { metadata } = metadataLookup
 
   const buildDefinition = getLookupCodecBuilder(metadataLookup)
 
-  const buildLayout = (node: Layout): Codec<any> => {
-    if ("root" in node) {
-      return buildLayout(node.root.layout)
-    }
-    if ("leaf" in node) {
-      return buildDefinition(node.leaf.ty)
-    }
-    if ("hash" in node) {
-      throw new Error("HashLayout not implemented")
-    }
-    if ("array" in node) {
-      return Vector(buildLayout(node.array.layout), node.array.len)
-    }
-    if ("struct" in node) {
-      return Struct(
-        Object.fromEntries(
-          node.struct.fields.map((field) => [
-            field.name,
-            buildLayout(field.layout),
-          ]),
-        ) as StringRecord<Codec<any>>,
-      )
-    }
+  const buildStorage = (name = "") => {
+    const storageEntry = metadataLookup.storage[name]
+    if (!storageEntry)
+      throw new Error(`Storage entry ${name ? name : "{root}"} not found`)
 
-    const variants = Object.values(node.enum.variants)
-    if (
-      node.enum.name === "Option" &&
-      variants.length === 2 &&
-      variants[0].name === "None" &&
-      variants[1].name === "Some"
-    ) {
-      const inner =
-        variants[1].fields.length === 1
-          ? buildLayout(variants[1].fields[0].layout)
-          : Tuple(...variants[1].fields.map((v) => buildLayout(v.layout)))
-      return Option(inner)
+    const keyCodec =
+      storageEntry.key == null ? _void : buildDefinition(storageEntry.key)
+    return {
+      key: prependBytes(keyCodec, storageEntry.keyPrefix),
+      value: buildDefinition(storageEntry.typeId),
     }
-
-    return Enum(
-      Object.fromEntries(
-        Object.values(node.enum.variants).map((variant) => [
-          variant.name,
-          Struct(
-            Object.fromEntries(
-              variant.fields.map((field) => [
-                field.name,
-                buildLayout(field.layout),
-              ]),
-            ) as StringRecord<Codec<any>>,
-          ),
-        ]),
-      ) as StringRecord<Codec<any>>,
-    )
   }
-  const buildStorageRoot = () => buildLayout(metadata.storage)
 
   const buildCallable = (callable: {
     selector: string
     args: Array<MessageParamSpec>
     returnType: TypeSpec
   }) => {
-    const selectorBytes = Binary.fromHex(callable.selector).asBytes()
     const argsCodec = Struct(
       Object.fromEntries(
         callable.args.map((param) => [
@@ -90,17 +44,9 @@ export const getInkDynamicBuilder = (metadataLookup: InkMetadataLookup) => {
         ]),
       ) as StringRecord<Codec<any>>,
     )
-    const callCodec = Tuple(Bytes(4), argsCodec)
 
     return {
-      call: enhanceCodec(
-        callCodec,
-        (value: CodecType<typeof argsCodec>): CodecType<typeof callCodec> => [
-          selectorBytes,
-          value,
-        ],
-        ([, value]) => value,
-      ),
+      call: prependBytes(argsCodec, callable.selector),
       value: buildDefinition(callable.returnType.type),
     }
   }
@@ -143,9 +89,22 @@ export const getInkDynamicBuilder = (metadataLookup: InkMetadataLookup) => {
   return {
     buildConstructor,
     buildMessage,
-    buildStorageRoot,
+    buildStorage,
     buildEvent,
   }
 }
 
 export type InkDynamicBuilder = ReturnType<typeof getInkDynamicBuilder>
+
+const prependBytes = <T>(codec: Codec<T>, hex: string) => {
+  const bytes = Binary.fromHex(hex).asBytes()
+  const wrappedCodec = Tuple(Bytes(bytes.length), codec)
+  return enhanceCodec(
+    wrappedCodec,
+    (value: CodecType<typeof codec>): CodecType<typeof wrappedCodec> => [
+      bytes,
+      value,
+    ],
+    ([, value]) => value,
+  )
+}
