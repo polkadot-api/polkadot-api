d9188ce1df30ec3b7fa66abaf29d395b03b928c6
Carlo Sala <carlosalag@protonmail.com>
2024-10-17 09:52:41 +0200

    make generation parallel

--- packages/docgen/src/generateDocs.ts
+++ packages/docgen/src/generateDocs.ts
@@ -48,56 +48,58 @@ export async function generateDocs(opts: Options) {
   await fs.rm(opts.output, { recursive: true, force: true })
   await fs.mkdir(opts.output, { recursive: true })
 
-  for (const chain of chains) {
-    console.log(`Generating TS descriptors for chain ${chain.key}`)
-    const chainDescriptorsPath = path.join(docsDescriptorsFolder, chain.key)
+  await Promise.all(
+    chains.map(async (chain) => {
+      console.log(`Generating TS descriptors for chain ${chain.key}`)
+      const chainDescriptorsPath = path.join(docsDescriptorsFolder, chain.key)
 
-    const result = await generateDocsDescriptors(chain.key, chain.metadata)
+      const result = await generateDocsDescriptors(chain.key, chain.metadata)
 
-    await fs.rm(chainDescriptorsPath, { recursive: true, force: true })
-    await fs.mkdir(chainDescriptorsPath, { recursive: true })
+      await fs.rm(chainDescriptorsPath, { recursive: true, force: true })
+      await fs.mkdir(chainDescriptorsPath, { recursive: true })
 
-    const tsconfigPath = path.join(chainDescriptorsPath, "tsconfig.json")
-    await fs.writeFile(tsconfigPath, getTsConfig(), "utf8")
-    await generateFileTree(chainDescriptorsPath, result)
+      const tsconfigPath = path.join(chainDescriptorsPath, "tsconfig.json")
+      await fs.writeFile(tsconfigPath, getTsConfig(), "utf8")
+      await generateFileTree(chainDescriptorsPath, result)
 
-    console.log(`Running typedoc for chain ${chain.key}`)
+      console.log(`Running typedoc for chain ${chain.key}`)
 
-    const cssFile = path.join(chainDescriptorsPath, "style.css")
-    await fs.writeFile(cssFile, getCss(), "utf-8")
+      const cssFile = path.join(chainDescriptorsPath, "style.css")
+      await fs.writeFile(cssFile, getCss(), "utf-8")
 
-    const typedoc = await TypeDoc.Application.bootstrapWithPlugins({
-      entryPoints: [path.join(chainDescriptorsPath, "index.ts")],
-      tsconfig: tsconfigPath,
-      titleLink: "/",
-      //@ts-ignore customTitle option comes from a plugin, not reflected in types
-      customTitle: "All chains",
-      cleanOutputDir: true,
-      customCss: cssFile,
-      name: chain.key,
-      readme: undefined,
-      plugin: ["typedoc-plugin-extras"],
-    })
+      const typedoc = await TypeDoc.Application.bootstrapWithPlugins({
+        entryPoints: [path.join(chainDescriptorsPath, "index.ts")],
+        tsconfig: tsconfigPath,
+        titleLink: "/",
+        //@ts-ignore customTitle option comes from a plugin, not reflected in types
+        customTitle: "All chains",
+        cleanOutputDir: true,
+        customCss: cssFile,
+        name: chain.key,
+        readme: undefined,
+        plugin: ["typedoc-plugin-extras"],
+      })
 
-    const project = await typedoc.convert()
-    if (!project) {
-      throw new Error(`Typedoc convert failed for chain ${chain.key}`)
-    }
+      const project = await typedoc.convert()
+      if (!project) {
+        throw new Error(`Typedoc convert failed for chain ${chain.key}`)
+      }
 
-    const chainOutDir = path.join(opts.output, chain.key)
-    await fs.mkdir(chainOutDir, { recursive: true })
-    await typedoc.generateDocs(project, chainOutDir)
+      const chainOutDir = path.join(opts.output, chain.key)
+      await fs.mkdir(chainOutDir, { recursive: true })
+      await typedoc.generateDocs(project, chainOutDir)
 
-    // index.html is currently useless, this is the easiest way to point to the
-    // right starting page
-    await fs.cp(
-      path.join(chainOutDir, "modules.html"),
-      path.join(chainOutDir, "index.html"),
-      { force: true },
-    )
+      // index.html is currently useless, this is the easiest way to point to the
+      // right starting page
+      await fs.cp(
+        path.join(chainOutDir, "modules.html"),
+        path.join(chainOutDir, "index.html"),
+        { force: true },
+      )
 
-    await fs.rm(chainDescriptorsPath, { recursive: true, force: true })
-  }
+      await fs.rm(chainDescriptorsPath, { recursive: true, force: true })
+    }),
+  )
 
   const indexFile = path.join(opts.output, "index.html")
   await fs.writeFile(
