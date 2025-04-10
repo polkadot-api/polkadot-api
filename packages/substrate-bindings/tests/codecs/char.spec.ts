2262dca4ea113de512c6cd57ad945f0a3a0aebb9
Carlo Sala <carlosalag@protonmail.com>
2025-04-10 13:03:20 +0200

    chore: update fast-tests calls

--- packages/substrate-bindings/tests/codecs/char.spec.ts
+++ packages/substrate-bindings/tests/codecs/char.spec.ts
@@ -3,9 +3,12 @@ import { expect, describe } from "vitest"
 import { char } from "@/."
 
 describe("char", () => {
-  it.prop([fc.char()])("should encode/decode chars", (input) => {
-    const encoded = char.enc(input)
-    expect(char.enc(input)).toStrictEqual(encoded)
-    expect(char.dec(encoded)).toStrictEqual(input)
-  })
+  it.prop([fc.string({ minLength: 1, maxLength: 1 })])(
+    "should encode/decode chars",
+    (input) => {
+      const encoded = char.enc(input)
+      expect(char.enc(input)).toStrictEqual(encoded)
+      expect(char.dec(encoded)).toStrictEqual(input)
+    },
+  )
 })
