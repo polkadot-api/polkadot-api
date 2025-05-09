0a7d71aa0ee9d1ff0ddf96f58e4b8462312b5631
Carlo Sala <carlosalag@protonmail.com>
2025-05-09 10:44:44 +0200

    defensive programming

--- packages/signers/ledger-signer/src/get-metadata.ts
+++ packages/signers/ledger-signer/src/get-metadata.ts
@@ -10,7 +10,7 @@ export const getMetadata = (
 ): NormalizedMetadata<15 | 16> => {
   try {
     const metadata = normalizeMetadata(decAnyMetadata(input))
-    if (metadata.version === 14) throw new Error("Wrong metadata version")
+    if (metadata.version <= 14) throw new Error("Wrong metadata version")
     return metadata as NormalizedMetadata<15 | 16>
   } catch (e) {
     throw e || new Error("Unable to decode metadata")
