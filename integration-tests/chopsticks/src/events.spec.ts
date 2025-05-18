d2091ed652082e4afe54f92558e312676b39dc8b
Victor Oliva <v.oliva@proton.me>
2025-05-18 16:07:20 +0200

    fix(event): `event.watch()` completing the observable on specific scenarios. (#1040)

---
+++ integration-tests/chopsticks/src/events.spec.ts
@@ -0,0 +1,27 @@
+import { paseo } from "@polkadot-api/descriptors"
+import { createClient } from "polkadot-api"
+import { describe, expect, it, vi } from "vitest"
+import { createBlock, getChopsticksProvider } from "./chopsticks"
+
+describe("events", () => {
+  it(".watch() reports events after awaiting for the initial block", async () => {
+    const client = createClient(getChopsticksProvider())
+    const api = client.getTypedApi(paseo)
+
+    await api.compatibilityToken
+
+    const testFn = vi.fn()
+    const completeFn = vi.fn()
+    api.event.System.ExtrinsicSuccess.watch().subscribe({
+      next: testFn,
+      complete: completeFn,
+    })
+
+    await createBlock(client)
+
+    expect(testFn).toHaveBeenCalled()
+    expect(completeFn).not.toHaveBeenCalled()
+
+    client.destroy()
+  })
+})
