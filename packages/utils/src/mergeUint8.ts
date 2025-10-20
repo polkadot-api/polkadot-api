5a0892e5078c88e03935ffab492a1db00fe2f7f7
Josep M Sobrepere <jm.sobrepere@gmail.com>
2025-10-20 16:10:01 +0300

    breaking(utils): remove depreated overload (#1187)

--- packages/utils/src/mergeUint8.ts
+++ packages/utils/src/mergeUint8.ts
@@ -1,17 +1,8 @@
-// TODO: deprecate spread overload in papi v2
-
 interface MergeUint8 {
-  /**
-   * @deprecated This overload will be removed in PAPI v2. Migrate as
-   *             follows:
-   *             mergeUint8(arr1, arr2) => mergeUint8([arr1, arr2])
-   */
-  (...inputs: Array<Uint8Array>): Uint8Array
   (inputs: Array<Uint8Array>): Uint8Array
 }
 
-export const mergeUint8: MergeUint8 = (...i) => {
-  const inputs = (Array.isArray(i[0]) ? i[0] : i) as Uint8Array[]
+export const mergeUint8: MergeUint8 = (inputs) => {
   const totalLen = inputs.reduce((acc, a) => acc + a.byteLength, 0)
   const result = new Uint8Array(totalLen)
 
