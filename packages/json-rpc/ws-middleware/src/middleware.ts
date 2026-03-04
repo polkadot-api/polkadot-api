7f00f617af64aaf9a925ff5ed833201c9f27debd
Josep M Sobrepere <jm.sobrepere@gmail.com>
2026-03-04 12:24:10 +0100

    fix(ws-provider): prevent non-archive RPCs to go though legacy middleware

--- packages/json-rpc/ws-middleware/src/middleware.ts
+++ packages/json-rpc/ws-middleware/src/middleware.ts
@@ -4,9 +4,11 @@ import { modern } from "./modern"
 import { withNumericIds } from "./numeric-ids"
 import { apply } from "./utils"
 
-const modernGroups = ["chainHead", "transaction", "chainSpec", "archive"].map(
-  (name) => `${name}_v1`,
-)
+const modernGroups = [
+  "chainHead",
+  "transaction",
+  "chainSpec" /*, "archive"*/,
+].map((name) => `${name}_v1`)
 const isModern = (methods: string[]): boolean =>
   modernGroups.every((group) => methods.some((m) => m.startsWith(group)))
 
