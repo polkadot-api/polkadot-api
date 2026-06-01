a1c12b43ade51eb627bce70d13f158b92d44d731
Carlo Sala <carlosalag@protonmail.com>
2026-06-01 12:36:47 +0200

    remove duplicated function

--- examples/bun/src/signer.ts
+++ examples/bun/src/signer.ts
@@ -9,11 +9,6 @@ import { getTxCreator } from "polkadot-api/signer"
 const miniSecret = entropyToMiniSecret(mnemonicToEntropy(DEV_PHRASE))
 const derive = sr25519CreateDerive(miniSecret)
 
-export const getDevSigner = (path = "//Alice") => {
-  const keyPair = derive(path)
-  return getTxCreator(keyPair.publicKey, "Sr25519", keyPair.sign)
-}
-
 export const getDevTxCreator = (path = "//Alice") => {
   const keyPair = derive(path)
   return getTxCreator(keyPair.publicKey, "Sr25519", keyPair.sign)
