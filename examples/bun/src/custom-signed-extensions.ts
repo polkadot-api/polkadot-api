81a16157d93e25fa0ca465cc1fcfc584bdd97546
Victor Oliva <v.oliva@proton.me>
2026-06-11 10:49:50 +0200

    fix custom signed extensions example

--- examples/bun/src/custom-signed-extensions.ts
+++ examples/bun/src/custom-signed-extensions.ts
@@ -15,22 +15,28 @@ const transfer = api.tx.Balances.transfer_allow_death({
 })
 
 transfer
-  .getEstimatedFees(papiTestCreator)
+  .getEstimatedFees(papiTestCreator, {
+    customSignedExtensions: { CheckAppId: { value: 0 } },
+  })
   .then((estimatedFees) => console.log({ estimatedFees }), console.error)
 
 // TODO: add custom signed-extension support to TxCreator-based flows.
-transfer.createSubmitAndWatch(papiTestCreator).subscribe({
-  next: (e) => {
-    console.log(e.type)
-    if (e.type === "finalized") {
-      console.log("The tx is now in a finalized block, check it out:")
-      console.log(
-        `https://explorer.avail.so/?rpc=wss%3A%2F%2Fturing-rpc.avail.so%2Fws#/explorer/query/${e.block.hash}`,
-      )
-    }
-  },
-  error: console.error,
-  complete() {
-    client.destroy()
-  },
-})
+transfer
+  .createSubmitAndWatch(papiTestCreator, {
+    customSignedExtensions: { CheckAppId: { value: 0 } },
+  })
+  .subscribe({
+    next: (e) => {
+      console.log(e.type)
+      if (e.type === "finalized") {
+        console.log("The tx is now in a finalized block, check it out:")
+        console.log(
+          `https://explorer.avail.so/?rpc=wss%3A%2F%2Fturing-rpc.avail.so%2Fws#/explorer/query/${e.block.hash}`,
+        )
+      }
+    },
+    error: console.error,
+    complete() {
+      client.destroy()
+    },
+  })
