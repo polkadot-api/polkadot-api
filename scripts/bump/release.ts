a62f285acda848da2ebdf7f66ab75eab9c6a6b88
Carlo Sala <carlosalag@protonmail.com>
2026-07-08 11:01:37 +0200

    scripts: `release` takes into account prerelease

--- scripts/bump/release.ts
+++ scripts/bump/release.ts
@@ -2,6 +2,9 @@ import { $ } from "bun"
 import { PACKAGES } from "./consts"
 import { CHLOG_HEADER, getPkgsWithChanges } from "./utils"
 
+const semver =
+  /^(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(?:-((?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+([0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?$/
+
 // ensure we are on root directory
 // cd to it
 const root = (await $`git rev-parse --show-toplevel`.quiet().text()).trim()
@@ -20,16 +23,20 @@ pkgsWithChanges.forEach(async (pkg) => {
     pkgJsonFile.text(),
   ])
   const parsedPkgJson = JSON.parse(pkgJson)
-  const parsedVersion = (parsedPkgJson.version as string)
-    .split(".")
-    .map((v) => parseInt(v))
-  if (chlog.slice(CHLOG_HEADER.length + 4).startsWith("Fixed"))
-    parsedVersion[2]!++
-  else {
-    parsedVersion[1]!++
-    parsedVersion[2] = 0
+  const parsedVersion = semver.exec(parsedPkgJson.version as string)
+  if (parsedVersion == null) throw new Error(`Version parsing of ${pkg} failed`)
+  const [_, maj, min, pat, pre] = parsedVersion
+  let newVersion
+  if (pre) {
+    const [tag, ver] = pre.split(".")
+    newVersion = `${maj}.${min}.${pat}-${tag}.${parseInt(ver!) + 1}`
+  } else {
+    if (chlog.slice(CHLOG_HEADER.length + 4).startsWith("Fixed"))
+      newVersion = `${maj}.${min}.${parseInt(pat!) + 1}`
+    else newVersion = `${maj}.${parseInt(min!) + 1}.0`
+    if (process.env.WITH_PRERELEASE)
+      newVersion += `-${process.env.WITH_PRERELEASE}.1`
   }
-  const newVersion = parsedVersion.join(".")
   parsedPkgJson.version = newVersion
   chlogFile.write(
     CHLOG_HEADER +
